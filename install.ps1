55d842c243a2d4ed63a092dd65dbff48edc69b7c
Lukas Dürrenberger <lukas.duerrenberger@zuehlke.com>
2022-02-21 16:10:52 +0100

    Add a basic install script on Windows

---
+++ install.ps1
@@ -0,0 +1,22 @@
+Param(
+    [string]$target = "Debug",
+    [string]$distributionTool = "C:/tools/DistributionTool.exe",
+    [string]$outputDir = "C:/temp"
+)
+
+Push-Location -Path "bin/$target/"
+
+$uuid = "dev.duerrenberger.clockify"
+
+Stop-Process -Name "StreamDeck" -Force -ErrorAction SilentlyContinue
+Stop-Process -Name "$uuid" -Force -ErrorAction SilentlyContinue
+
+Start-Sleep -Seconds 2
+
+Remove-Item -Path "$outputDir/$uuid.streamDeckPlugin" -Recurse -Force -ErrorAction SilentlyContinue
+Start-Process "$distributionTool" -ArgumentList "--build --input $uuid.sdPlugin --output $outputDir" -NoNewWindow -Wait
+Remove-Item -Path "$env:APPDATA/Elgato/StreamDeck/Plugins/$uuid.sdPlugin" -Recurse -Force -ErrorAction SilentlyContinue
+
+Invoke-Item -Path "$outputDir/$uuid.streamDeckPlugin"
+
+Pop-Location
