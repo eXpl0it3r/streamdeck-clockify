5ffe3d0021bd9d9dcc21df32b6c74cb3c5f0c2ef
Lukas Dürrenberger <eXpl0it3r@my-gate.net>
2025-06-23 21:31:00 +0200

    Add billable option

--- Clockify/ClockifyContext.cs
+++ Clockify/ClockifyContext.cs
@@ -26,6 +26,7 @@ public class ClockifyContext
     private string _taskName = string.Empty;
     private string _timerName = string.Empty;
     private string _workspaceName = string.Empty;
+    private bool _billable = true;
 
     public ClockifyContext(Logger logger)
     {
@@ -66,7 +67,8 @@ public class ClockifyContext
             UserId = _currentUser.Id,
             WorkspaceId = workspace.Id,
             Description = _timerName ?? string.Empty,
-            Start = DateTimeOffset.UtcNow
+            Start = DateTimeOffset.UtcNow,
+            Billable = _billable
         };
 
         if (!string.IsNullOrEmpty(_projectName))
@@ -183,6 +185,7 @@ public class ClockifyContext
         _taskName = settings.TaskName;
         _timerName = settings.TimerName;
         _clientName = settings.ClientName;
+        _billable = settings.Billable;
     }
 
     private async Task ReloadCacheAsync()
--- Clockify/PluginSettings.cs
+++ Clockify/PluginSettings.cs
@@ -22,6 +22,9 @@ public class PluginSettings
     [JsonProperty(PropertyName = "clientName")]
     public string ClientName { get; set; } = string.Empty;
 
+    [JsonProperty(PropertyName = "billable")]
+    public bool Billable { get; set; } = true;
+
     [JsonProperty(PropertyName = "titleFormat")]
     public string TitleFormat { get; set; } = string.Empty;
 
